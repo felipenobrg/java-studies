45436a395367cf53f071359e562725338095d5cc
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-02-29 16:40:36 -0300

    feat: adding string methods example

---
+++ src/StringMethods/App.java
@@ -0,0 +1,22 @@
+package StringMethods;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String name = "     Bro   ";
+
+        // boolean result = name.equalsIgnoreCase(name);
+        // int result = name.length();
+        // char result = name.charAt(0);
+        // int result = name.indexOf("o");
+        // boolean result = name.isEmpty();
+        // String result = name.toUpperCase();
+        // String result = name.toUpperCase();
+        // String result = name.replace();
+        String result = name.trim();
+
+        System.out.println(result);
+
+    }
+}
